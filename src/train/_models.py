9e0cdd598294d0cfb5ef8f8e612b02d268239c75
Matt Ricci <mattgricci@gmail.com>
2022-10-21 15:01:58 +0300

    Added AE architecture

--- src/train/_models.py
+++ src/train/_models.py
@@ -27,6 +27,8 @@ def load_model(model_type, pretrained_path=None, device='cpu', **kwargs):
 
     if model_type == 'CNNwFC_exp_emb':
         model = CNNwFC_exp_emb(**kwargs)
+    elif model_type == 'Conv2dAE':
+        model = Conv2dAE(**kwargs)
     else:
         raise ValueError("Haven't added other models yet!")
 
@@ -35,17 +37,18 @@ def load_model(model_type, pretrained_path=None, device='cpu', **kwargs):
     return model.to(device)
 
 class CNNwFC(nn.Module):
-    def __init__(self, in_shape, num_DE_params,
+    def __init__(self, in_shape, poly_order=3, latent_dim=64,
             num_conv_layers=1, kernel_sizes=[5], kernel_features=[16],
             pooling_sizes=[],
             strides = [1],
             num_fc_hid_layers=0, fc_hid_dims=[],
-            batch_norm=False, dropout=False, dropout_rate=.5, activation_type='relu',
-            conditional=False, return_activations=False, finetune=False):
+            min_dims=[-1.,-1.],
+            max_dims=[1.,1.],
+            finetune=False, batch_norm=False, dropout=False, dropout_rate=.5, activation_type='relu',last_pad=None):
 
         super(CNNwFC, self).__init__()
         self.finetune = finetune
-        self.conv = CNN(in_shape, num_conv_layers=num_conv_layers,
+        self.enc = CNN(in_shape, num_conv_layers=num_conv_layers,
                         kernel_sizes=kernel_sizes, kernel_features=kernel_features,
                         pooling_sizes = pooling_sizes,
                         strides = strides,
@@ -54,11 +57,20 @@ class CNNwFC(nn.Module):
 
         with torch.no_grad():
             x = torch.rand(1, *in_shape)
-            y = self.conv(x)
+            y = self.enc(x)
             self.out_shape = y.shape[1:]
             self.out_size = torch.prod(torch.tensor(y.shape))
 
-        self.fc = MLP(self.out_size, num_DE_params,
+        self.poly_order = poly_order
+
+        spatial_coords = [np.linspace(mn, mx, self.num_lattice) for (mn, mx) in zip(min_dims, max_dims)]
+        mesh = np.meshgrid(*spatial_coords)
+        self.L = torch.tensor(np.concatenate([ms[..., None] for ms in mesh], axis=-1))
+        
+        library, library_terms = sindy_library(self.L.reshape(self.num_lattice**self.dim,self.dim), poly_order)
+        self.library = library.float()
+
+        self.dec = MLP(self.out_size, num_DE_params,
                          num_hid_layers=num_fc_hid_layers, hid_dims=fc_hid_dims,
                          batch_norm=batch_norm, dropout=dropout, dropout_rate=dropout_rate,
                          activation_type=activation_type)
@@ -66,9 +78,9 @@ class CNNwFC(nn.Module):
     def forward(self,x):
         if self.finetune:
             with torch.no_grad():
-                x = self.conv(x)
+                x = self.enc(x)
         else:
-            x = self.conv(x)
+            x = self.enc(x)
         x = x.reshape(-1, self.out_size)
         return self.fc(x)
 
@@ -80,15 +92,14 @@ class CNNwFC_exp_emb(nn.Module):
             num_fc_hid_layers=0, fc_hid_dims=[],
             min_dims=[-1.,-1.],
             max_dims=[1.,1.],
-            batch_norm=False, dropout=False, dropout_rate=.5, activation_type='relu',
-            conditional=False, return_activations=False, finetune=False):
+            batch_norm=False, dropout=False, dropout_rate=.5, activation_type='relu', last_pad=None):
 
         super(CNNwFC_exp_emb, self).__init__()
 
         self.dim         = in_shape[0]
         self.num_lattice = in_shape[1]
         self.latent_dim = latent_dim
-        self.conv = CNN(in_shape, num_conv_layers=num_conv_layers,
+        self.enc = CNN(in_shape, num_conv_layers=num_conv_layers,
                         kernel_sizes=kernel_sizes, kernel_features=kernel_features,
                         pooling_sizes = pooling_sizes,
                         strides = strides,
@@ -97,7 +108,7 @@ class CNNwFC_exp_emb(nn.Module):
 
         with torch.no_grad():
             x = torch.rand(1, *in_shape)
-            y = self.conv(x)
+            y = self.env(x)
             self.out_shape = y.shape[1:]
             self.out_size = torch.prod(torch.tensor(y.shape))
 
@@ -115,16 +126,70 @@ class CNNwFC_exp_emb(nn.Module):
         library, library_terms = sindy_library(self.L.reshape(self.num_lattice**self.dim,self.dim), poly_order)
         self.library = library.float()
 
-        self.fc = MLP(latent_dim, self.dim * len(library_terms),
-                         num_hid_layers=num_fc_hid_layers, hid_dims=fc_hid_dims,
-                         batch_norm=batch_norm, dropout=dropout, dropout_rate=dropout_rate,
-                         activation_type=activation_type)
+        self.dec = MLP(latent_dim, self.dim * len(library_terms),
+                       num_hid_layers=num_fc_hid_layers, hid_dims=fc_hid_dims,
+                       batch_norm=batch_norm, dropout=dropout, dropout_rate=dropout_rate,
+                       activation_type=activation_type)
 
     def forward(self,x):
         if self.finetune:
             with torch.no_grad():
-                x = self.conv(x)
+                x = self.enc(x)
         else:
-            x = self.conv(x)
+            x = self.enc(x)
         x = x.reshape(-1, self.out_size)
         return self.fc(self.emb(x))
+
+class Conv2dAE(nn.Module):
+    def __init__(self, in_shape, latent_dim=64,
+            num_conv_layers=1, kernel_sizes=[5], kernel_features=[16],
+            pooling_sizes=[],
+            strides = [1],
+            num_fc_hid_layers=0, fc_hid_dims=[],
+            batch_norm=False, dropout=False, dropout_rate=.5, activation_type='relu',
+            finetune=False,last_pad=False, min_dims=None, max_dims=None, poly_order=None):
+
+        super(Conv2dAE, self).__init__()
+        self.finetune = finetune
+        self.latent_dim = latent_dim
+        self.enc = CNN(in_shape, num_conv_layers=num_conv_layers,
+                        kernel_sizes=kernel_sizes, kernel_features=kernel_features,
+                        pooling_sizes = pooling_sizes,
+                        strides = strides,
+                        batch_norm=batch_norm, dropout=dropout, dropout_rate=dropout_rate,
+                        activation_type=activation_type)
+
+        with torch.no_grad():
+            x = torch.rand(1, *in_shape)
+            y = self.enc(x)
+            self.out_shape = y.shape[1:]
+            self.out_size = torch.prod(torch.tensor(y.shape))
+
+        self.emb   = MLP(self.out_size, latent_dim,
+                         num_hid_layers=0, hid_dims=[],
+                         batch_norm=batch_norm, dropout=dropout, dropout_rate=dropout_rate,
+                         activation_type=activation_type)
+
+        self.deemb = MLP(latent_dim, self.out_size,
+                         num_hid_layers=0, hid_dims=[],
+                         batch_norm=batch_norm, dropout=dropout, dropout_rate=dropout_rate,
+                         activation_type=activation_type)
+
+        deconv_kernel_features = kernel_features[::-1][:-1] + [in_shape[0]]
+        self.deconv = dCNN(self.out_shape, num_conv_layers=num_conv_layers,
+                           kernel_sizes=kernel_sizes[::-1], kernel_features=deconv_kernel_features,
+                           pooling_sizes = pooling_sizes[::-1],
+                           strides = strides,
+                           batch_norm=batch_norm, dropout=dropout, dropout_rate=dropout_rate,
+                           activation_type=activation_type,
+                           last_pad=last_pad)
+
+        self.dec = lambda x : self.deconv(self.deemb(x).reshape(-1,*self.out_shape))
+
+    def forward(self,x):
+        if self.finetune:
+            with torch.no_grad():
+                x = self.enc(x)
+        else:
+            x = self.enc(x)
+        return self.dec(x)
